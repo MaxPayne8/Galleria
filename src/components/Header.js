8348f6bd5731e2d3e37663da999313fc925fd56f
MaxPayne8 <maxpayne0852.com>
2023-12-29 04:37:22 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="w-full bg-slate-200 text-center flex">
       <Link to="/">
         <img
-          className="rounded-lg  w-14"
+          className="rounded-lg  w-14 ml-2"
           src="https://png.pngtree.com/png-vector/20190521/ourlarge/pngtree-gallery-logo-icon-design-template-vector-png-image_1048779.jpg"
           alt="img"
           onClick={() => {
